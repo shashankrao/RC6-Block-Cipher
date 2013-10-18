54f790adf98a487b6b0a7c2d751baf6bd2a88946
rjoker <rohit.knight@gmail.com>
2013-10-18 13:50:05 +0530

    RC6 Encrpytion

---
+++ encrypt.py
@@ -0,0 +1,102 @@
+import math
+
+def ROR(x, n, bits = 32):
+    mask = (2L**n) - 1
+    mask_bits = x & mask
+    return (x >> n) | (mask_bits << (bits - n))
+
+def ROL(x, n, bits = 32):
+    return ROR(x, bits - n,bits)
+
+def blockConverter(sentence):
+    encoded = []
+    res = ""
+    for i in range(0,len(sentence)):
+        if i%4==0 and i!=0 :
+            encoded.append(res)
+            res = ""
+        temp = bin(ord(sentence[i]))[2:]
+        if len(temp) <8:
+            temp = "0"*(8-len(temp)) + temp
+        res = res + temp
+    encoded.append(res)
+    return encoded
+
+def getBlocksBinary(sentence):
+    return blockConverter(sentence)
+
+def generateKey(userkey):
+    r=10
+    w=32
+    b=len(userkey)
+    modulo = 2**32
+    s=(2*r+4)*[0]
+    s[0]=0xB7E15163
+    for i in range(1,2*r+4):
+        s[i]=(s[i-1]+0x9E3779B9)%(2**w)
+    encoded = getBlocksBinary(userkey)
+    print encoded
+    enlength = len(encoded)
+    l = enlength*[0]
+    for i in range(1,enlength+1):
+        l[enlength-i]=long(encoded[i-1],2)
+    
+    v = 3*max(enlength,2*r+4)
+    A=B=i=j=0
+    
+    for index in range(0,v):
+        A = s[i] = ROL((s[i] + A + B)%modulo,3,32)
+        B = l[j] = ROL((l[j] + A + B)%modulo,(A+B)%32,32) 
+        i = (i + 1) % (2*r + 4)
+        j = (j + 1) % enlength
+    return s
+    
+        
+def encrypt(sentence,s):
+    encoded = getBlocksBinary(sentence)
+    enlength = len(encoded)
+    A = long(encoded[0],2)
+    B = long(encoded[1],2)
+    C = long(encoded[2],2)
+    D = long(encoded[3],2)
+    orgi = []
+    orgi.append(A)
+    orgi.append(B)
+    orgi.append(C)
+    orgi.append(D)
+    print "Orginal String List", orgi
+    r=10
+    w=32
+    modulo = 2**32
+    lgw = 5
+    B = (B + s[0])%modulo
+    D = (D + s[1])%modulo 
+    for i in range(1,r+1):
+        t_temp = (B*(2*B + 1))%modulo 
+        t = ROL(t_temp,lgw,32)
+        u_temp = (D*(2*D + 1))%modulo
+        u = ROL(u_temp,lgw,32)
+        tmod=t%32
+        umod=u%32
+        A = (ROL(A^t,umod,32) + s[2*i])%modulo 
+        C = (ROL(C^u,tmod,32) + s[2*i+ 1])%modulo
+        temp = A
+        A = B
+        B = C
+        C = D
+        D = temp
+
+    A = (A + s[2*r + 2])%modulo 
+    C = (C + s[2*r + 3])%modulo
+    cipher = []
+    cipher.append(A)
+    cipher.append(B)
+    cipher.append(C)
+    cipher.append(D)
+    return cipher
+
+
+s = generateKey('A WORD IS A WORD')
+sentence = 'I WORD IS A WORD'
+cipher = encrypt(sentence,s)
+print "Encrypted String List ",cipher
